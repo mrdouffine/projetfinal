5a20957abe567bf0846ba5c0b18dca64df9e52a3
kojhack <122357318+kojhack@users.noreply.github.com>
2025-08-23 14:41:59 +0000

    attribute authorize correct

--- GestionConge/obj/Debug/net9.0/GestionConge.AssemblyInfo.cs
+++ GestionConge/obj/Debug/net9.0/GestionConge.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionConge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1115924f85f5767219c90cbaf1c2cca78c73892")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34d1a91da65e1f6e926c8063b58b24d7fde68fcf")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionConge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionConge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
