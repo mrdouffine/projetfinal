24f77b37b774461ec5b8dbbde4fc8f39acb32417
kojhack <122357318+kojhack@users.noreply.github.com>
2025-09-02 18:05:50 +0000

    validations handle2

--- GestionConge.Client/obj/Debug/net9.0/GestionConge.Client.AssemblyInfo.cs
+++ GestionConge.Client/obj/Debug/net9.0/GestionConge.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionConge.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88a685aa98b195a9e3363f14b08ba3e2f756b196")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8bc037cfa5682917c4f27cd5384b1f728df3d4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionConge.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionConge.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
